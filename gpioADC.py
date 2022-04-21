9fffe6eba3e42e249663f37a2d8e5b4ba013a38e
Nicolai Høirup Nielsen <55984156+niconielsen32@users.noreply.github.com>
2022-04-21 15:35:54 +0200

    Update gpioADC.py

--- gpioADC.py
+++ gpioADC.py
@@ -6,12 +6,6 @@ import ADC0832 as ADC
 # Create an ADC0832 instance
 adc = ADC.ADC0832()
 
-# Specify which GPIO pins will be used
-adc.csPin = 24 # Default pin: 17
-adc.clkPin = 23 # Default pin: 27
-adc.doPin = 19 # Default pin: 23
-adc.diPin = 21 # Default pin: 24
-
  
 potentiometer_channel = 0
 pressure_sensor_channel = 1
